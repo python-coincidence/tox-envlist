761fa0b840af2b35f6c30897b9b69db5a3fe47bf
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-28 21:23:13 +0000

    Updated files with 'repo_helper'.

--- setup.py
+++ setup.py
@@ -12,13 +12,10 @@ sys.path.append('.')
 # this package
 from __pkginfo__ import *  # pylint: disable=wildcard-import
 
-
-
 setup(
 		description='Allows selection of a different tox envlist.',
 		extras_require=extras_require,
 		install_requires=install_requires,
 		py_modules=[],
 		version=__version__,
-
 		)
